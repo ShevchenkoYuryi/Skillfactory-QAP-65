f375f740bbbe96ff207a52547da02ec022034b26
ShevchenkoYuryi <shevsyi@gmail.com>
2022-03-29 23:36:50 +0400

    file optimization

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,28 +1,3 @@
-ticket = int(input("Количество  приобретаебых билетов - "))
-price=[]
-for i in range(1, ticket+1):
-   age = int(input('Введите ваш возраст - '))
-   if age < 18:
-      price.append(0)
-   elif 18 <= age <= 25:
-      price.append(990)
-   else:
-      price.append(1390)
-print('Стоимость билетов ВСЕГО =', sum(price))
-if ticket >= 3:
-   a = sum(price)*10/100
-   print("Сумма вашей скидки (10%) -", a)
-   print("Итого к оплате -", sum(price)-a)
-else:
-   print("Сумма вашей скидки (0%) - 0")
-   print("Итого к оплате -", sum(price))
+path = '/home/user/documents/file.txt'
 
-
-
-# if ticket > 3:
-#     a = int(sum(price) - sum(price)/10)
-#     print("your purchase amount is:" , a)
-# else:
-#     a = sum(price)
-#     print ("your purchase amount is: ", a)
-# print(price)
+print(path.split('/')) # разделитель можно указать в качестве аргумента
