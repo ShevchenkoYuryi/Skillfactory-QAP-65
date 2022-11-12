cb42792eea2c3caf59849531cc7ce548f75a5d6d
ShevchenkoYuryi <shevsyi@gmail.com>
2022-11-12 20:50:07 +0400

    Add all tets in file "test_calc" for Calculator

--- Module_19/tests/test_calc.py
+++ Module_19/tests/test_calc.py
@@ -6,8 +6,20 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_exponentiation(self, ):
-        assert self.calc.exponentiation(self, 2, 3)
+    def test_exponentiation(self):
+        assert self.calc.exponentiation(self, 3, 2) == 9
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 3, 2) == 6
+
+    def test_division(self):
+        assert self.calc.division(self, 6, 2) == 3
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 3, 2) == 1
+
+    def test_adding(self):
+        assert self.calc.adding(self, 2, 3) == 5
 
     def teardown(self):
         print('Выполнение метода Teardown')
