9330a8949a357da41cdf0617e1207400fd10e745
ShevchenkoYuryi <shevsyi@gmail.com>
2023-02-06 23:48:19 +0400

    Add homework module 25

---
+++ Module_25/tests/test_pet_card.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture(autouse=True)
+def testing():
+   pytest_driver = webdriver.Chrome('D:\__Programming\_Python_My_Projects\WebDriver_Se\chromedriver_win32\chromedriver.exe')
+
+   '''Открывем сайт в браузере Chrome'''
+   pytest_driver.get('http://petfriends.skillfactory.ru/login')
+
+   yield pytest_driver
+   pytest_driver.quit()
+
+
+def test_search_example(testing):
+
+    driver = testing
+
+    '''Вводим email'''
+    driver.find_element(By.ID, 'email').send_keys('shevsyi@yandex.ru')
+    '''Вводим пароль'''
+    driver.find_element(By.ID, 'pass').send_keys('061786&pkm_pchZH')
+    '''Нажимаем на кнопку входа в аккаунт'''
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"
+
+    '''Переходим в папку Мои питомцы'''
+    driver.find_element(By.XPATH, '//button[@type="button"]').click()
+    driver.find_element(By.XPATH, '//a[text()="Мои питомцы"]').click()
+
+    '''В личном кабинете формируем количество наших питомцев'''
+    number_pets = driver.find_elements(By.TAG_NAME, 'tr')
+    '''В личном кабинете формируем количество фото питомцев'''
+    images = driver.find_elements(By.TAG_NAME, 'img')
+    '''В личном кабинете формируем список имен питомцев'''
+    names = driver.find_elements(By.TAG_NAME, 'td')
+
+    assert len(number_pets) == len(images)
--- PythonProject/Webinars.py
+++ PythonProject/Webinars.py
@@ -1,119 +1,119 @@
 
                                           # МОДУЛЬ 16 ООП и классы в Python
-# import math
+import math
 # 1 TASK
-# print('ВВедите длины сторон треугольника')
+print('ВВедите длины сторон треугольника')
 
-# a = int(input('a = '))
-# b = int(input('b = '))
-# c = int(input('c = '))
-# p = a + b + c / 2
-# s = math.sqrt (p * (p - a) * (p - b) * (p - c))
-# print ("P = %d; S=%.2f" % (p, s))
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+p = a + b + c / 2
+s = math.sqrt (p * (p - a) * (p - b) * (p - c))
+print ("P = %d; S=%.2f" % (p, s))
 
 # 2 TASK
-# n = int(input("Введите трехзначное число: "))
-# num1 = n % 10
-# num2 = n % 100 // 10
-# num3 = n // 100
-# print("Сумма цифр: ", num1 + num2 + num3)
-# print("Произведение цифр: ", num1 * num2 * num3)
+n = int(input("Введите трехзначное число: "))
+num1 = n % 10
+num2 = n % 100 // 10
+num3 = n // 100
+print("Сумма цифр: ", num1 + num2 + num3)
+print("Произведение цифр: ", num1 * num2 * num3)
 
 
 # 3 TASK
-# a = int(input('a='))
-# b = int(input('b='))
-# c = int(input('c='))
+a = int(input('a='))
+b = int(input('b='))
+c = int(input('c='))
 
-# m = a
-# if m < b:
-#   m = b
-# if m < c:
-#   m = c
-# print (m)
+m = a
+if m < b:
+  m = b
+if m < c:
+  m = c
+print (m)
 
 # 4 TASK
-# a = int(input('a='))
-# b = int(input('b='))
-# c = int(input('c='))
-# if b < a < c or c < a < b:
-#   print("Среднее:", a)
-# elif a < b < c or c < b < a:
-#   print("Среднее:", b)
-# else:
-#   print("Среднее:", c)
+a = int(input('a='))
+b = int(input('b='))
+c = int(input('c='))
+if b < a < c or c < a < b:
+  print("Среднее:", a)
+elif a < b < c or c < b < a:
+  print("Среднее:", b)
+else:
+  print("Среднее:", c)
 
 
 # 5 TASK
 
-# import random
+import random
 
-# num = random.randint(1, 100)
+num = random.randint(1, 100)
 
-# while True:
-#   print("Угадайте число от 1 до 100")
-#   guess = int(input())
-#   if guess == num:
-#     print ('Правильно')
-#     break
-#   elif guess < num:
-#     print('Загаданное число больше')
-#   elif guess > num:
-#     print('Загаданное число меньше')
+while True:
+  print("Угадайте число от 1 до 100")
+  guess = int(input())
+  if guess == num:
+    print ('Правильно')
+    break
+  elif guess < num:
+    print('Загаданное число больше')
+  elif guess > num:
+    print('Загаданное число меньше')
 
 # 6 TASK (Сумма чисел из числа)
-# num = int(input('Введите целое число: '))
-# num1 = 0
+num = int(input('Введите целое число: '))
+num1 = 0
 
-# while num > 0:
-#   digit = num % 10
-#   num = num // 10
-#   num1 = num1 * 10
-#   num1 = num1 + digit
-# print(num1)
+while num > 0:
+  digit = num % 10
+  num = num // 10
+  num1 = num1 * 10
+  num1 = num1 + digit
+print(num1)
 
 
 # 6 TASK (количество четных и не четных чисел)
-# a = int(input())
-# even = 0
-# odd = 0
+a = int(input())
+even = 0
+odd = 0
 
-# while a > 0:
-#   if a % 2 == 0:
-#     even += 1
-#   else:
-#     odd += 1
-#   a = a // 10
-# print("Even: %d, odd: %d" % (even, odd))
-
-
-# 7 TASK (рекурсия)
-# n = int(input())
-# factorial = 1
-# while n > 1:
-#   factorial = factorial * n
-#   n -= 1
-# print(factorial)
-
-# n = int(input())
-# factorial = 1
-# for i in range(1, n+1):
-#   factorial = factorial * i
-# print(factorial)
+while a > 0:
+  if a % 2 == 0:
+    even += 1
+  else:
+    odd += 1
+  a = a // 10
+print("Even: %d, odd: %d" % (even, odd))
 
 
-# 8 TASK (таблица умножения)
-# for i in range(1, 10):
-#   for j in range(1, 10):
-#     print(i * j, end='\t')
-#   print('\n')
+7 TASK (рекурсия)
+n = int(input())
+factorial = 1
+while n > 1:
+  factorial = factorial * n
+  n -= 1
+print(factorial)
+
+n = int(input())
+factorial = 1
+for i in range(1, n+1):
+  factorial = factorial * i
+print(factorial)
 
 
-# 9 TASK (циклы в списках)
-# lst = []
-# for item in range(18, 1, -4):
-#     lst.append(item)
-# print(lst)
+8 TASK (таблица умножения)
+for i in range(1, 10):
+  for j in range(1, 10):
+    print(i * j, end='\t')
+  print('\n')
+
+
+9 TASK (циклы в списках)
+lst = []
+for item in range(18, 1, -4):
+    lst.append(item)
+print(lst)
 
 s = 10000 ** 2
 d = 10000 * 13.288
